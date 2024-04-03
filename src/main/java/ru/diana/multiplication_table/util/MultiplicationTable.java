a7fe4c0cc74c8442b08e9249d299f5e38e740a2a
DianaGL96 <gorina.diana1996@gmail.com>
2024-04-03 13:32:55 +0300

    Changes after verification

--- src/main/java/ru/diana/multiplication_table/util/MultiplicationTable.java
+++ src/main/java/ru/diana/multiplication_table/util/MultiplicationTable.java
@@ -2,12 +2,10 @@ package ru.diana.multiplication_table.util;
 
 public class MultiplicationTable {
     public void startMultiplication(int number) {
-        boolean errorDisplayed = false;
         if (number < 2 || number > 5) {
             System.out.println("Ошибка! Вы ввели число вне допустимого диапазона.");
-            errorDisplayed = true;
         }
-        if (!errorDisplayed) {
+        else {
             System.out.println("Таблица умножения для числа " + number + "\n");
             for (int i = 1; i <= 10; i++) {
                     int product = number * i;
